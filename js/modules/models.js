bd8e8dab35a11035e4ee65ea113b86b59b84e002
MDx3R <nationmdxr@gmail.com>
2025-01-13 22:27:55 +0300

    add order model and getPrice for product model

--- js/modules/models.js
+++ js/modules/models.js
@@ -11,8 +11,31 @@ class Product {
         this.created_at = json.created_at;
         this.container = null;
     }
+
+    getPrice() {
+        return this.discount_price ?? this.actual_price;
+    }
+}
+
+class Order {
+    constructor(json) {
+        this.id = json.id;
+        this.full_name = json.full_name;
+        this.email = json.email;
+        this.phone = json.phone;
+        this.student_id = json.student_id;
+        this.delivery_address = json.delivery_address;
+        this.delivery_date = json.delivery_date;
+        this.delivery_interval = json.delivery_interval;
+        this.good_ids = json.good_ids;
+        this.comment = json.comment;
+        this.subscribe = json.subscribe;
+        this.created_at = json.created_at;
+        this.updated_at = json.updated_at;
+    }
 }
 
 export {
     Product,
+    Order,
 };
