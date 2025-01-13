3c50399cf70cfcc67d1083f80fa6300f391ccf55
MDx3R <nationmdxr@gmail.com>
2025-01-13 15:05:57 +0300

    connect js to index.html

---
+++ js/pages/catalog.js
@@ -0,0 +1,19 @@
+import {
+    ProductRepository,
+} from "../modules/repositories.js";
+import {
+    NotificationView,
+    ProductView
+} from "../modules/views.js";
+import {
+    ProductController,
+} from "../modules/controllers.js";
+
+const productRepository = new ProductRepository();
+const productView = new ProductView();
+const notificationView = new NotificationView();
+const productController = new ProductController(
+    productRepository, 
+    productView, 
+    notificationView
+);
