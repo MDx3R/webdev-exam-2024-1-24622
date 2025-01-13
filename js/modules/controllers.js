86e2c3153628d4bfbfc5e813ce4e707568ca0e09
MDx3R <nationmdxr@gmail.com>
2025-01-13 22:37:19 +0300

    add cart and checkout controller

--- js/modules/controllers.js
+++ js/modules/controllers.js
@@ -1,3 +1,9 @@
+import {
+    formatDate,
+} from "./serializers.js";
+import {
+    todayDate,
+} from "./utils.js";
 
 class ProductController {
     constructor(repository, view, notification) {
@@ -5,14 +11,14 @@ class ProductController {
         this.view = view;
         this.notification = notification;
 
-        // binds
+        this.makeBinds();
+    }
 
+    makeBinds() {
         this.view.bindSort(this.handleSort.bind(this));
         this.view.bindFilter(this.handleFilter.bind(this));
         this.view.bindAddToCart(this.handleAddToCart.bind(this));
         this.view.bindRemoveFromCart(this.handleRemoveFromCart.bind(this));
-
-        this.init();
     }
 
     async init() {
@@ -38,23 +44,162 @@ class ProductController {
         this.view.renderProducts(sortedProducts);
     }
 
-    handleFilter(filter) {
+    handleFilter(filter) {}
 
-    }
+    clearCart() {}
 
     handleAddToCart(product) {
         this.repository.addToCart(product);
         this.view.toggleProduct(product);
-        console.log("added to cart: " + product.name);
     }
 
     handleRemoveFromCart(product) {
         this.repository.removeFromCart(product);
         this.view.toggleProduct(product);
-        console.log("removed to cart: " + product.name);
+    }
+}
+
+class CartController extends ProductController {
+    constructor(controller, repository, view, notification) {
+        super(repository, view, notification);
+        
+        this.controller = controller; // checkout
+    }
+
+    makeBinds() {
+        this.view.bindRemoveFromCart(this.handleRemoveFromCart.bind(this));
+    }
+
+    async loadProducts() {
+        await this.repository.loadCart();
+        this.view.renderProducts(
+            this.repository.products, 
+            this.repository.cart
+        );      
+    }
+
+    clearCart() {
+        this.repository.clearCart();
+        this.repository.clearProducts();
+        this.view.removeProducts();
+        this.controller.handleClearCart();
+    }
+
+    handleRemoveFromCart(product) {
+        this.repository.removeFromCart(product);
+        this.repository.removeFromProducts(product);
+        this.view.removeProduct(product);
+        this.controller.handleRemoveFromCart(product);
+    }
+}
+
+class CheckoutController {
+    constructor(
+        productRepository, 
+        orderRepository, 
+        view, 
+        notification
+    ) {
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+        this.view = view;
+        this.notification = notification;
+        
+        this.cartController = null;
+
+        this.makeBinds();
+    }
+
+    makeBinds() {
+        this.view.bindCreateOrder(this.handleCreateOrder.bind(this));
+        this.view.bindChangeDate(this.handleChangeDate.bind(this));
+        this.view.bindChangeTime(this.handleChangeTime.bind(this));
+    }
+
+    init() {
+        this.loadCheckout();
+    }
+
+    loadCheckout() {
+        this.view.renderCheckout(this.productRepository.products);
+    }
+
+    handleCreateOrder(data) {
+        let jsonData = {};
+
+        for (const [key, value] of data.entries()) {
+            if (key != "comment" && !value) {
+                this.notification.showNotification(
+                    "Заполните все поля", 
+                    "Неправильные данные."
+                );
+                return;
+            }
+            if (key == "delivery_date") {
+                if (todayDate() > new Date(value)) {
+                    this.notification.showNotification(
+                        "Время доставки должно быть после текущего времени", 
+                        "Неправильное время."
+                    );
+                    return;
+                }
+                jsonData[key] = formatDate(value);
+                continue;
+            }
+            if (key == "subscribe") {
+                jsonData[key] = value == "on";
+                continue;
+            }
+            jsonData[key] = value;
+        }
+
+        if (this.productRepository.cart.length == 0) {
+            this.notification.showNotification(
+                "Добавьте товары перед оформлением заказа", 
+                "Нет товаров."
+            );
+            return;
+        }
+
+        jsonData["good_ids"] = this.productRepository.cart;
+
+        this.orderRepository.createOrder(JSON.stringify(jsonData))
+            .then(() => {
+                this.notification.showNotification(
+                    "Заказ был успешно создан", 
+                    "success"
+                );
+
+                this.cartController.clearCart();
+                this.view.clearCheckout();
+            })
+            .catch((error) => {
+                this.notification.showNotification(
+                    error.message, 
+                    error.name
+                );
+            });
+    }
+
+    handleRemoveFromCart(product) {
+        this.loadCheckout();
+    }
+
+    handleClearCart(product) {
+        this.loadCheckout();
+    }
+
+    handleChangeDate() {
+        this.loadCheckout();
+    }
+
+    handleChangeTime() {
+        this.loadCheckout();
     }
 }
 
 export {
-    ProductController
+    ProductController,
+    CartController,
+    CheckoutController,
 };
