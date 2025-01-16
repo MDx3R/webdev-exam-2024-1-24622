9c84f7ae4560d7fed45231405cba26b4e2eeb9af
MDx3R <nationmdxr@gmail.com>
2025-01-16 19:42:36 +0300

    add controller methods for handling loading products by pages and account controller

--- js/modules/controllers.js
+++ js/modules/controllers.js
@@ -19,10 +19,18 @@ class ProductController {
         this.view.bindFilter(this.handleFilter.bind(this));
         this.view.bindAddToCart(this.handleAddToCart.bind(this));
         this.view.bindRemoveFromCart(this.handleRemoveFromCart.bind(this));
+        this.view.bindLoadMore(this.handleLoadMore.bind(this));
     }
 
     async init() {
+        this.repository.getCart();
         await this.loadProducts();
+    }
+
+    async initByPage() {
+        this.repository.getCart();
+        this.view.showLoadMoreButton();
+        await this.loadProductsByPage();
     }
 
     async loadProducts() {
@@ -32,6 +40,26 @@ class ProductController {
                 this.repository.products, 
                 this.repository.cart
             );
+        } catch (err) {
+            this.notification.showNotification(err.message, err.name);
+        }   
+    }
+
+    async loadProductsByPage(page = null) {
+        if (page) {
+            this.view.removeProducts();
+        }
+        try {
+            let start = await this.repository.getProductsByPage(page);
+            this.view.addProducts(
+                this.repository.products.slice(start), 
+                this.repository.cart
+            );
+            if (!this.repository.areProductsLeft()) {
+                this.view.hideLoadMoreButton();
+            } else {
+                this.view.showLoadMoreButton();
+            }
         } catch (err) {
             this.notification.showNotification(err.message, err.name);
         }
@@ -57,6 +85,10 @@ class ProductController {
         this.repository.removeFromCart(product);
         this.view.toggleProduct(product);
     }
+
+    handleLoadMore() {
+        this.loadProductsByPage();
+    }
 }
 
 class CartController extends ProductController {
@@ -71,11 +103,15 @@ class CartController extends ProductController {
     }
 
     async loadProducts() {
-        await this.repository.loadCart();
-        this.view.renderProducts(
-            this.repository.products, 
-            this.repository.cart
-        );      
+        try {
+            await this.repository.loadCart();
+            this.view.renderProducts(
+                this.repository.products, 
+                this.repository.cart
+            );  
+        } catch (err) {
+            this.notification.showNotification(err.message, err.name);
+        }   
     }
 
     clearCart() {
@@ -165,13 +201,21 @@ class CheckoutController {
 
         this.orderRepository.createOrder(JSON.stringify(jsonData))
             .then(() => {
-                this.notification.showNotification(
-                    "Заказ был успешно создан", 
-                    "success"
-                );
-
                 this.cartController.clearCart();
                 this.view.clearCheckout();
+
+                localStorage.setItem(
+                    'notification', 
+                    "Заказ был успешно создан:success"
+                );
+
+                let path = window.location.pathname;
+                let pathParts = path.split('/');
+                pathParts.pop();
+                let basePath = pathParts.join('/');
+
+                let newLocation = basePath + "/index.html";
+                window.location.href = newLocation; 
             })
             .catch((error) => {
                 this.notification.showNotification(
@@ -198,8 +242,115 @@ class CheckoutController {
     }
 }
 
+class AccountController {
+    constructor(repository, view, notification) {
+        this.repository = repository;
+        this.view = view;
+        this.notification = notification;
+
+        this.makeBinds();
+    }
+
+    async init() {
+        await this.loadOrders();
+    }
+
+    async loadOrders() {
+        try {
+            await this.repository.getOrders();
+            this.view.renderOrders(this.repository.orders);  
+        } catch (err) {
+            this.notification.showNotification(err.message, err.name);
+        }
+    }
+
+    makeBinds() {
+        this.view.bindRejectAction(this.handleRejectAction.bind(this));
+        // this.view.bindViewOrder(this.handleViewOrder.bind(this));
+        this.view.bindEditOrder(this.handleEditOrder.bind(this));
+        this.view.bindRemoveOrder(this.handleRemoveOrder.bind(this));
+    }
+
+    // handleViewOrder() {}
+
+    handleRejectAction() {
+        this.notification.showCancelActionNotification();
+    }
+
+    handleEditOrder(order, data) {
+        let jsonData = {};
+
+        for (const [key, value] of data.entries()) {
+            if (key != "comment" && !value) {
+                this.notification.showNotification(
+                    "Заполните все поля", 
+                    "Неправильные данные."
+                );
+                return;
+            }
+            if (key == "delivery_date") {
+                if (todayDate() > new Date(value)) {
+                    this.notification.showNotification(
+                        "Время доставки должно быть после текущего времени", 
+                        "Неправильное время."
+                    );
+                    return;
+                }
+                jsonData[key] = formatDate(value);;
+                order[key] = value;
+                continue;
+            }
+            if (key == "subscribe") {
+                let subscribe = value == "on";
+                jsonData[key] = subscribe;
+                order[key] = subscribe;
+                continue;
+            }
+            jsonData[key] = value;
+            order[key] = value;
+        }
+
+        console.log(jsonData);
+
+        this.repository.updateOrder(order.id, JSON.stringify(jsonData))
+            .then(() => {
+                this.notification.showNotification(
+                    "Заказ был успешно изменен", 
+                    "success"
+                );
+
+                this.view.updateOrder(order);
+            })
+            .catch((error) => {
+                this.notification.showNotification(
+                    error.message, 
+                    error.name
+                );
+            });
+    }
+
+    handleRemoveOrder(order) {
+        this.repository.removeOrder(order.id)
+            .then(() => {
+                this.notification.showNotification(
+                    "Заказ был успешно удален", 
+                    "success"
+                );
+                this.repository.removeFromOrders(order);
+                this.view.removeOrder(order);
+            })
+            .catch((error) => {
+                this.notification.showNotification(
+                    error.message, 
+                    error.name
+                );
+            });
+    }
+}
+
 export {
     ProductController,
     CartController,
     CheckoutController,
+    AccountController,
 };
