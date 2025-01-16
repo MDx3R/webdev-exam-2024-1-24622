d39b796fb1727db59124c5324db92f9438fdd368
MDx3R <nationmdxr@gmail.com>
2025-01-16 19:37:34 +0300

    add datetime formatting function

--- js/modules/serializers.js
+++ js/modules/serializers.js
@@ -9,6 +9,20 @@ function formatDate(date) {
     return date.toLocaleDateString("de-DE", options).replace(',', '');
 }
 
+function formatDateTime(datetime) {
+    const options = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    };
+    let date = new Date(datetime);
+    return date.toLocaleDateString("de-DE", options).replace(',', '');
+}
+
 export {
     formatDate,
+    formatDateTime,
 };
